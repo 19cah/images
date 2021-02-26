89f79d4be2f8006b9b111d08c0bddf90107625a0
Abraham Hernandez <abraham@abranhe.com>
2021-02-25 20:19:43 -0500

    fixing pipeline

--- index.js
+++ index.js
@@ -1,71 +1,69 @@
 const axios = require('axios');
 const qs = require('query-string');
 
-async function tweet({ token, message = 'Hello World', ct0, auth_token, ...options }) {
-  if (!token || !ct0 || !auth_token) {
-    throw Error('Missing required parameters');
-  }
+class TweetSomethingError extends Error {}
 
-  const data = qs.stringify({
-    skip_status: '1',
-    include_cards: '1',
-    include_can_dm: '1',
-    include_blocking: '1',
-    include_mute_edge: '1',
-    include_blocked_by: '1',
-    include_reply_count: '1',
-    include_followed_by: '1',
-    include_want_retweets: '1',
-    include_can_media_tag: '1',
-    include_profile_interstitial_type: '1',
-    batch_mode: 'off',
-    trim_user: 'false',
-    tweet_mode: 'extended',
-    cards_platform: 'Web-12',
-    include_quote_count: 'true',
-    simple_quoted_tweet: 'true',
-    include_ext_alt_text: 'true',
-    include_ext_media_color: 'true',
-    auto_populate_reply_metadata: 'false',
-    include_ext_media_availability: 'true',
-    status: message,
-    ...options,
-  });
+async function tweet({token, message = 'Hello World', ct0, authToken, ...options}) {
+	if (!token || !ct0 || !authToken) {
+		throw new TweetSomethingError('Missing required parameters');
+	}
 
-  const config = {
-    method: 'post',
-    url: 'https://twitter.com/i/api/1.1/statuses/update.json',
-    headers: {
-      Authorization: `Bearer ${token}`,
-      authority: 'twitter.com',
-      'accept-encoding': 'gzip, deflate, br',
-      'accept-language': 'en-US,en;q=0.9',
-      'content-length': '504',
-      'content-type': 'application/x-www-form-urlencoded',
-      cookie: `auth_token=${auth_token}; ct0=${ct0};`,
-      origin: 'https://twitter.com',
-      referer: 'https://twitter.com/compose/tweet',
-      'sec-fetch-dest': 'empty',
-      'sec-fetch-mode': 'cors',
-      'sec-fetch-site': 'same-origin',
-      'sec-gpc': '1',
-      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
-      'x-csrf-token': ct0,
-      'x-twitter-active-user': 'yes',
-      'x-twitter-auth-type': 'OAuth2Session',
-      'x-twitter-client-language': 'en',
-    },
-    data: data,
-  };
+	const data = qs.stringify({
+		skip_status: '1',
+		include_cards: '1',
+		include_can_dm: '1',
+		include_blocking: '1',
+		include_mute_edge: '1',
+		include_blocked_by: '1',
+		include_reply_count: '1',
+		include_followed_by: '1',
+		include_want_retweets: '1',
+		include_can_media_tag: '1',
+		include_profile_interstitial_type: '1',
+		batch_mode: 'off',
+		trim_user: 'false',
+		tweet_mode: 'extended',
+		cards_platform: 'Web-12',
+		include_quote_count: 'true',
+		simple_quoted_tweet: 'true',
+		include_ext_alt_text: 'true',
+		include_ext_media_color: 'true',
+		auto_populate_reply_metadata: 'false',
+		include_ext_media_availability: 'true',
+		status: message,
+		...options
+	});
 
-  return await axios(config)
-    .then(data => {
-      console.log(data.data)
-    })
-    .catch((error) => {
-      console.log('error', error)
-      new Error(`Something went wrong. ${error}`)
-    });
+	const config = {
+		method: 'post',
+		url: 'https://twitter.com/i/api/1.1/statuses/update.json',
+		data,
+		headers: {
+			Authorization: `Bearer ${token}`,
+			authority: 'twitter.com',
+			'accept-encoding': 'gzip, deflate, br',
+			'accept-language': 'en-US,en;q=0.9',
+			'content-length': '504',
+			'content-type': 'application/x-www-form-urlencoded',
+			cookie: `auth_token=${authToken}; ct0=${ct0};`,
+			origin: 'https://twitter.com',
+			referer: 'https://twitter.com/compose/tweet',
+			'sec-fetch-dest': 'empty',
+			'sec-fetch-mode': 'cors',
+			'sec-fetch-site': 'same-origin',
+			'sec-gpc': '1',
+			'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+			'x-csrf-token': ct0,
+			'x-twitter-active-user': 'yes',
+			'x-twitter-auth-type': 'OAuth2Session',
+			'x-twitter-client-language': 'en'
+		}
+	};
+
+	// @todo it needs to verify if the tweet was actually sent, otherwise throw an error
+	return await axios(config)
+		.then(res => res.data)
+		.catch(error => new TweetSomethingError(error));
 }
 
 module.exports = tweet;
--- readme.md
+++ readme.md
@@ -1,6 +1,6 @@
 # tweet-something
 
-[![Github](https://github.com/abranhe/tweet-something/workflows/build/badge.svg)](https://github.com/abranhe/tweet-something)
+[![build](https://github.com/abranhe/tweet-something/actions/workflows/ci.yml/badge.svg)](https://github.com/abranhe/tweet-something/actions/workflows/ci.yml)
 [![Travis](https://travis-ci.com/abranhe/tweet-something.svg?branch=master)](https://travis-ci.com/abranhe/tweet-something)
 [![npm](https://img.shields.io/npm/v/tweet-something.svg?logo=npm)](https://npmjs.org/tweet-something)
 
@@ -15,18 +15,18 @@ npm install tweet-something
 ## Usage
 
 ```js
-import tweetSomethig from 'tweet-something';
+import tweetSomethig from "tweet-something";
 
 (async () => {
-  const config = {
-    message: 'Hello Everyone',
-    token: '****************',
-    auth_token: '*************',
-    ct0: '*************',
-  };
+	const config = {
+		message: "Hello Everyone",
+		token: "****************",
+		authToken: "*************",
+		ct0: "*************",
+	};
 
-  await tweetSomethig(config);
-  // tweet sent
+	await tweetSomethig(config);
+	// tweet sent
 })();
 ```
 
@@ -41,7 +41,7 @@ Type: `Object`
 The required parameters to send a tweet.
 
 - `token`: All of the endpoints requires OAuth2 Authorizations. Note that they refuse OAuth2 Bearer tokens obtained from POST `oauth2/token`.
-- `auth_token`: You can get it from your browser's cookie
+- `authToken`: You can get it from your browser's cookie
 - `ct0`: You can get it from your browser's cookie
 
 If you want to read more about those values, [check this](https://gist.github.com/stepney141/c161a83f02c42e161c905249733b9225).
